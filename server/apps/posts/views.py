c34e737c9d18126fa98478a30beea20aac822929
minseo jang <mingdo.dev@gmail.com>
2023-08-02 01:48:54 +0900

    #4 feat: 게시글 목록/작성/삭제 기능 구현

--- server/apps/posts/views.py
+++ server/apps/posts/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Happy, Sad
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 
+# 편의를 위해 변수명을 s만 붙인 형태로 지었어요..
 def board(request):
     posts = Post.objects.all()
-    #happy
-    #sad
+    happys = Happy.objects.all()
+    sads = Sad.objects.all()
 
-    return render(request, "#",{"posts":posts})
+    return render(request, "#",{"posts":posts, "happys":happys, "sads":sads})
 
 
-# 새 게시글 버튼 누르면 작성 창 등장
-
-# 작성 누르면 비동기로 게시글 생성
+# 새 게시글 버튼 누르면 작성 창 등장?
+# 작성 버튼 누르면 게시글 생성
 # 게시글 : content, room_id(FK), created_at, user_id(현재 로그인 user)
+# request : room_id, text, request.user(현재 로그인 user의 id)
 @csrf_exempt
 def create_ajax(request):
     req = json.loads(request.body)
-    user_id = req['user_id']
     room_id = req['room_id']
     text = req['text']
+    user_id = req['user_id']
 
     if text:
         post = Post.objects.create(
@@ -32,8 +33,32 @@ def create_ajax(request):
             created_at = timezone.now(),
         )
         post.save()
+        happy = Happy.objects.create(
+            post_id = post,
+            user_id = user_id,
+        )
+        happy.save()
+        sad = Sad.objects.create(
+            post_id = post,
+            user_id = user_id,
+        )
+        sad.save()
 
-        return JsonResponse({'post':post, 'text':text})
+        return JsonResponse({'post':post, 'happy':happy, 'sad':sad})
 
-def post_delete(request):
-    pass
+# 삭제 버튼 누르면 게시글 삭제
+# 일단 '삭제되었습니다!' alert로 띄우고 자동 reload되면 게시글이 사라지는 걸로 생각
+@csrf_exempt
+def delete_ajax(request):
+    req = json.loads(request.body)
+    post_id = req['post_id']
+    post =  Post.objects.get(id=post_id)
+    post.delete()
+
+    return JsonResponse()
+
+# @csrf_exempt
+# def happy_ajax(request):
+#     req = json.loads(request.body)
+#     post_id =req['post_id']
+#     post = Post.objects.get(id=post_id)
