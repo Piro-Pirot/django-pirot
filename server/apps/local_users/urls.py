369efc0c90bed06e5290595b08e304d94e684713
minseo jang <mingdo.dev@gmail.com>
2023-08-03 12:04:05 +0900

    #8 feat: 일반회원 프로필 수정 기능 구현

---
+++ server/apps/local_users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from server.apps.local_users.views import *
+
+urlpatterns = [
+    path("/user/setting/", profile_setting),
+]
--- server/apps/local_users/views.py
+++ server/apps/local_users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from server.apps.channels.models import Staff
 
-# Create your views here.
+
+# 일반회원 : 프로필 설정 페이지 / 운영진 : 운영진 페이지
+def profile_setting(request):
+
+    current_user = request.user
+
+    # 운영진 여부
+    if Staff.objects.filter(user=current_user).exists():
+        return redirect("/staff/passer_create/level/")
+
+    if request.method == "POST":
+        current_user.profile_img = request.POST["profile_img"]
+        current_user.save()
+
+        return redirect("/user/setting/") # 프로필 설정 페이지에 머무름
+    
+    return render(request, "#", {"user":current_user})
