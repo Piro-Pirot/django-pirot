c1ef47b186181885a37843104a49f08d93100439
SSunwo <aidnsunwoo@naver.com>
2023-08-03 20:32:57 +0900

    #18 feat: section4,5 & footer 추가

--- server/config/settings.py
+++ server/config/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
