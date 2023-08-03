0f68357abe2782f0d872e560f98ccd4bb81e97a2
ywonchae1 <ywonchae62@gmail.com>
2023-08-03 16:21:17 +0900

    #1 feat: mongodb 말풍선 내용 저장 함수 구현

--- server/apps/bubbles/mongodb.py
+++ server/apps/bubbles/mongodb.py
@@ -2,16 +2,19 @@ from pymongo import MongoClient
 import datetime
 from django.conf import settings
 
+# MongoDB와 원격 연결
 client = MongoClient(getattr(settings, 'MONGO_HOST', None), getattr(settings, 'MONGO_PORT', None))
 db = client.pirotchatdb
 
-def test():
+# collection 생성
+
+def save_msg(data):
     bubble_collection = db.bubble
 
     bubble = {
-        "user": "Mike",
-        "room": "b123",
-        "content": 'hello mongo',
+        "user": data['user'],
+        "room": data['roomUUID'],
+        "content": data['msg'],
         "is_delete": 0,
         "read_cnt": 2,
         "created_at": datetime.datetime.now(tz=datetime.timezone.utc),
