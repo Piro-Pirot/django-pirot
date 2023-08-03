50bbf949be83082519af352a0c6fea1c2212d61c
ywonchae1 <ywonchae62@gmail.com>
2023-08-03 16:22:08 +0900

    #1 feat: 채팅 방 접속 라우팅, view 함수 구현

--- server/apps/chat/urls.py
+++ server/apps/chat/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', test)
+    path('', test),
+    path('<uuid:pk>/', enter_room),
 ]
--- server/apps/chat/views.py
+++ server/apps/chat/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-import server.apps.bubbles.mongodb as mongodb
+from server.apps.chat.models import Room
 
 # Create your views here.
 
 def test(request):
-    mongodb.test()
     print('success')
-    return render(request, 'base.html')
+    return render(request, 'room.html', {'title': 'home'})
+
+def enter_room(request, pk):
+    print(request)
+    curRoom = Room.objects.get(pk=pk)
+    print(curRoom.room_name)
+    return render(
+        request,
+        'room.html',
+        {
+            'title': curRoom.room_name,
+            'room_uuid': pk
+        }
+    )
