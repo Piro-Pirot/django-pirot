665f74f5d1ff378a2b7a0e40c7c024e1cf12740a
ywonchae1 <ywonchae62@gmail.com>
2023-08-11 23:25:36 +0900

    #70 test: 생성된 정적 파일들

--- server/staticfiles/js/chat/friendBtn.js
+++ server/staticfiles/js/chat/friendBtn.js
@@ -27,9 +27,11 @@ window.addEventListener("click", (event) => {
 
 //친구들 프로필 호버 했을 떄 더보기 버튼 뜨기
 const friendContainer = document.querySelectorAll(".friend-container");
+
 friendContainer.forEach(container => {
   const moreButton = container.querySelector("#more");
   const moreForm = container.querySelector(".more-form");
+
   container.addEventListener("mouseover", () => {
     moreButton.style.opacity = "1";
   });
@@ -40,6 +42,25 @@ friendContainer.forEach(container => {
     moreForm.classList.toggle("active");
     container.classList.toggle("colored");
   });
+
+  const bookmarkButton = container.querySelector("#bookmark");
+  const bookmarkIcon = container.querySelector("#star");
+  bookmarkButton.addEventListener("click", (event) => {
+    if (bookmarkIcon.classList.contains("active")) {
+      bookmarkIcon.classList.toggle("active");
+      bookmarkButton.value = '즐겨찾기';
+      bookmarkAjax(event.classList);
+    }
+    else {
+      bookmarkIcon.classList.toggle("active");
+      bookmarkButton.value = '즐겨찾기 해제';
+      let friends = document.querySelector('.btn-friend-list');
+      friends.removeChild(container);
+      friends.insertBefore(container, friends.firstChild);
+      bookmarkDelAjax(event.classList);
+    }
+  });
+
   window.addEventListener("click", (event) => {
     if(moreForm.classList.contains("active")) {
       if (!container.contains(event.target)) {
@@ -52,3 +73,42 @@ friendContainer.forEach(container => {
 });
 
 
+/* 즐겨찾기 Ajax */
+try {
+  let cookie = document.cookie;
+  let csrfToken = cookie.substring(cookie.indexOf('=') + 1);
+} catch {
+  console.log('already defined');
+}
+
+const bookmarkAjax = async(target) => {
+  const url = '/staff/bookmark_ajax/';
+  const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'X-CSRFToken': csrfToken
+      },
+      body: JSON.stringify({target: target})
+  });
+  
+  if (res.ok) {
+    let response = await res.json();
+  }
+}
+
+const bookmarkDelAjax = async(target) => {
+  const url = '/staff/bookmark_del_ajax/';
+  const res = await fetch(url, {
+      method: 'POST',
+      header: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'X-CSRFToken': csrfToken
+      },
+      body: JSON.stringify({target: target})
+  });
+  
+  if (res.ok) {
+    let response = await res.json();
+  }
+}
