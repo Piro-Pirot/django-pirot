8c547486f4a119c9f620f3e25ba96c93b4d2ba07
hyeinjeong <jhi2002@hanyang.ac.kr>
2023-08-21 01:35:27 +0900

    #122 fix

--- server/config/settings.py
+++ server/config/settings.py
@@ -67,6 +67,8 @@ INSTALLED_APPS = [
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        
+        'markdownx',
         ]
 
 MIDDLEWARE = [
