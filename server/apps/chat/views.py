c8048c7569b04e5a517abf6177d38209714f628d
ywonchae1 <ywonchae62@gmail.com>
2023-08-21 07:29:47 +0900

    #204 fix: 채팅방 제거, 익명채팅방 누구누구 나갔습니다안내 오류 해결
    채팅방에 남아있는 사람이 없을 때 채팅방이 삭제되어야 함
    익명채팅방은 익명으로 나가져야 함

--- server/apps/chat/views.py
+++ server/apps/chat/views.py
@@ -176,7 +176,7 @@ def exit_room(request):
             BlindRoomMember.objects.get(user=request.user, room=cur_room).delete()
             # 채팅 방에 아무도 없으면 채팅 방 삭제
             try:
-                BlindRoomMember.objects.filter(room=cur_room).first()
+                BlindRoomMember.objects.get(room=cur_room)
             except BlindRoomMember.DoesNotExist:
                 cur_room.delete()
         else:
@@ -337,6 +337,8 @@ def enter_room(request, channelId, roomId, type):
         myPassInfo = Passer.objects.filter(passer_name=request.user.name, channel=curChannel).last()
         # 현재 로그인 사용자의 채널 구성원들
         myFriends = Passer.objects.filter(channel=curChannel).exclude(id=myPassInfo.id).order_by('-level', 'passer_name')
+
+        my_blind_info = ''
 
         if curRoom.room_type == BLIND_ROOM:
             #익명채팅방
